6f75119b5d46aa50adc3f3a9f6279eacc3a69c65
cruz-f <fernandocruz184@gmail.com>
2021-06-08 11:44:21 +0100

    [ADD] base for node; base for version node; register relationships between the two nodes

---
+++ src/protrend/models/node.py
@@ -0,0 +1,41 @@
+from typing import Union, Type, TYPE_CHECKING
+
+from neomodel import (UniqueIdProperty, RelationshipTo, DateTimeProperty)
+
+if TYPE_CHECKING:
+    from protrend.models.version import VersionNode
+
+
+class Node:
+    uid = UniqueIdProperty()
+    created = DateTimeProperty(default_now=True)
+
+    def __init_subclass__(cls,
+                          version: Type['VersionNode'] = None,
+                          to_: bool = False,
+                          from_: bool = False,
+                          **kwargs):
+
+        if version:
+
+            if to_:
+                setattr(cls, 'version', RelationshipTo(version, 'VERSIONING'))
+
+            if from_:
+                version.register(cls)
+
+    @classmethod
+    def name(cls):
+        return cls.__name__.lower()
+
+    def connect_version(self, version: Union[None, VersionNode]):
+
+        if version is None:
+            return
+
+        if hasattr(self, 'version'):
+            self.version.connect(version)
+
+        if hasattr(version, self.name()):
+            relationship = getattr(version, self.name())
+            relationship.connect(self)
