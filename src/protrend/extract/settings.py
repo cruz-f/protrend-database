651588a097ecaba146100720f8857caa72c373fe
cruz-f <fernandocruz184@gmail.com>
2021-06-16 16:21:25 +0100

    [REFACTOR] bot name

--- src/protrend/extract/settings.py
+++ src/protrend/extract/settings.py
@@ -7,7 +7,7 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'extract'
+BOT_NAME = 'ProTReND-Extract'
 
 SPIDER_MODULES = ['extract.spiders']
 NEWSPIDER_MODULE = 'extract.spiders'
@@ -25,7 +25,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
