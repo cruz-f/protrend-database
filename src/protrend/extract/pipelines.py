b811db0fb2d6385d694512a8fd2b6e2d9d5be7bf
cruz-f <fernandocruz184@gmail.com>
2021-06-18 09:01:15 +0100

    [ADD] RegPrecise pipeline

--- src/protrend/extract/pipelines.py
+++ src/protrend/extract/pipelines.py
@@ -5,6 +5,8 @@
 
 
 # useful for handling different item types with a single interface
+from protrend.extract.databases import RegPreciseDB
+from protrend.models.regprecise import Version as RegPreciseVersion
 from protrend.models.version import VersionNode
 from protrend.utils.db_connection import DBSettings
 
@@ -25,13 +27,13 @@ class NeoPipeline:
             user_name = 'db'
 
         if not password:
-            user_name = 'db'
+            password = 'db'
 
         if not ip:
-            user_name = 'localhost'
+            ip = 'localhost'
 
         if not port:
-            user_name = '7687'
+            port = '7687'
 
         self._user_name = user_name
         self._password = password
@@ -78,6 +80,82 @@ class NeoPipeline:
         return self._database
 
     def open_spider(self, spider):
+        pass
+
+    def close_spider(self, spider):
+        pass
+
+    def process_item(self, item, spider):
+        return item
+
+
+class RegPrecisePipeline(NeoPipeline):
+
+    def __init__(self,
+                 user_name: str = None,
+                 password: str = None,
+                 ip: str = None,
+                 port: str = None,
+                 version: str = None,
+                 clear_version: bool = False,
+                 clear_sa: bool = False,
+                 clear_sa_schema: bool = False):
+
+        if not user_name:
+            user_name = 'regprecise'
+
+        if not password:
+            password = 'regprecise'
+
+        if not ip:
+            ip = 'localhost'
+
+        if not port:
+            port = '7687'
+
+        super().__init__(user_name=user_name,
+                         password=password,
+                         ip=ip,
+                         port=port,
+                         version=version,
+                         clear_version=clear_version,
+                         clear_sa=clear_sa,
+                         clear_sa_schema=clear_sa_schema)
+
+    @property
+    def version(self) -> RegPreciseVersion:
+
+        if self._version is None:
+            versions = RegPreciseVersion.nodes.order_by('-created')
+
+            if versions:
+                self._version = versions[0]
+
+            else:
+                self._version = RegPreciseVersion(name='0.0.0').save()
+
+        if isinstance(self._version, str):
+            try:
+                self._version = RegPreciseVersion.nodes.get(name=self._version)
+
+            except RegPreciseVersion.DoesNotExist:
+
+                self._version = RegPreciseVersion(name=self._version).save()
+
+        return self._version
+
+    @property
+    def database(self) -> RegPreciseDB:
+
+        if self._database is None:
+            self._database = RegPreciseDB(user_name=self._user_name,
+                                          password=self._password,
+                                          ip=self._ip,
+                                          port=self._port)
+
+        return self._database
+
+    def open_spider(self, spider):
 
         self.database.connect()
 
@@ -95,8 +173,6 @@ class NeoPipeline:
             for node in children:
                 node.delete()
 
-    def close_spider(self, spider):
-        pass
-
     def process_item(self, item, spider):
+
         return item
--- src/protrend/run_spiders.py
+++ src/protrend/run_spiders.py
@@ -1,26 +1,51 @@
 from scrapy import cmdline
 
 
-def run_spider(spider: str, log_file: str, urls: str = None):
+def run_spider(spider: str,
+               log_file: str,
+               urls: str = None,
+               clear_version: bool = False,
+               clear_sa: bool = False,
+               clear_sa_schema: bool = False):
+
     if urls:
-        cmdline.execute(f"scrapy crawl {spider} -a urls={urls} --logfile {log_file}".split())
+        cmdline.execute(f"scrapy crawl {spider} "
+                        f"-a urls={urls} "
+                        f"--logfile {log_file} "
+                        f"-s clear_version={clear_version} "
+                        f"-s clear_sa={clear_sa} "
+                        f"-s clear_sa_schema={clear_sa_schema} ".split())
 
     else:
-        cmdline.execute(f"scrapy crawl {spider}".split())
+        cmdline.execute(f"scrapy crawl {spider} "
+                        f"--logfile {log_file} "
+                        f"-s clear_version={clear_version} "
+                        f"-s clear_sa={clear_sa} "
+                        f"-s clear_sa_schema={clear_sa_schema} ".split())
 
 
-def run_regprecise(urls: str = None):
+def run_reg_precise(urls: str = None,
+                    clear_version: bool = False,
+                    clear_sa: bool = False,
+                    clear_sa_schema: bool = False):
+
     log_file = 'regprecise.log'
 
-    run_spider("regprecise", log_file, urls)
+    run_spider("regprecise",
+               log_file,
+               urls,
+               clear_version,
+               clear_sa,
+               clear_sa_schema)
 
 
 if __name__ == "__main__":
-    regprecise_urls = ("https://regprecise.lbl.gov/collections_tax.jsp",
-                       "https://regprecise.lbl.gov/collections_tf.jsp",
-                       "https://regprecise.lbl.gov/collections_tffam.jsp",
-                       "https://regprecise.lbl.gov/collections_rfam.jsp",
-                       "https://regprecise.lbl.gov/collections_effector.jsp",
-                       "https://regprecise.lbl.gov/collections_pathway.jsp")
+    reg_precise_urls = ("https://regprecise.lbl.gov/collections_tax.jsp",
+                        "https://regprecise.lbl.gov/collections_tf.jsp",
+                        "https://regprecise.lbl.gov/collections_tffam.jsp",
+                        "https://regprecise.lbl.gov/collections_rfam.jsp",
+                        "https://regprecise.lbl.gov/collections_effector.jsp",
+                        "https://regprecise.lbl.gov/collections_pathway.jsp")
 
-    run_regprecise(regprecise_urls[0])
+    run_reg_precise(reg_precise_urls[0],
+                    clear_sa_schema=True)
