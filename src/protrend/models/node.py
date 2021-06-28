d527f00effb2dbcbc160ba5c09c6473cd08c6ba9
cruz-f <fernandocruz184@gmail.com>
2021-06-28 15:31:55 +0100

    [ADD] node properties, to dict and to series or dataframe

--- src/protrend/models/node.py
+++ src/protrend/models/node.py
@@ -1,5 +1,6 @@
 from typing import TYPE_CHECKING, Any, Union, Callable
 
+import pandas as pd
 from neomodel import (UniqueIdProperty, DateTimeProperty, StructuredNode)
 
 if TYPE_CHECKING:
@@ -22,19 +23,42 @@ class Node(StructuredNode):
         return cls.__name__
 
     @classmethod
-    def properties(cls):
+    def cls_properties(cls):
 
         return dict(cls.__all_properties__)
 
     @classmethod
-    def relationships(cls):
+    def cls_keys(cls):
+        return cls.cls_properties().keys()
 
-        return dict(cls.__all_relationships__)
+    @classmethod
+    def cls_values(cls):
+        return cls.cls_properties().values()
+
+    @classmethod
+    def cls_items(cls):
+        return cls.cls_properties().items()
+
+    @classmethod
+    def cls_to_dict(cls):
+
+        res = {key: [] for key in cls.cls_keys()}
+
+        for node in cls.nodes.all():
+            for key, val in node.properties.items():
+                if key in res:
+                    res[key].append(val)
+
+        return res
+
+    @classmethod
+    def cls_to_df(cls):
+        return pd.DataFrame.from_dict(cls.cls_to_dict())
 
     @classmethod
     def from_item(cls, item: dict, save: bool = True) -> StructuredNode:
         properties = {attr: value for attr, value in item.items()
-                      if attr in cls.properties()}
+                      if attr in cls.cls_properties()}
 
         instance = cls(**properties)
 
@@ -53,14 +77,19 @@ class Node(StructuredNode):
             if node.version.is_connected(version):
                 return node
 
+    @property
+    def properties(self):
+        return {key: val for key, val in self.__properties__.items()
+                if val is not None}
+
     def update(self, item: dict, save: bool = True) -> 'Node':
 
         properties = {attr: value for attr, value in item.items()
-                      if attr in self.properties()}
+                      if attr in self.cls_properties()}
 
         for attr, value in item.items():
 
-            if attr in self.properties():
+            if attr in self.cls_properties():
 
                 setattr(self, attr, value)
 
@@ -79,3 +108,18 @@ class Node(StructuredNode):
         if hasattr(version, rel_name):
             relationship = getattr(version, rel_name)
             relationship.connect(self)
+
+    def keys(self):
+        return self.properties.keys()
+
+    def values(self):
+        return self.properties.values()
+
+    def items(self):
+        return self.properties.items()
+
+    def to_dict(self):
+        return self.properties
+
+    def to_series(self):
+        return pd.Series(data=self.values(), index=self.keys())
