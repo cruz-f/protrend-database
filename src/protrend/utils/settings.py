c5c4c44c7375e48cc92416816847ce07404dd6fa
cruz-f <fernandocruz184@gmail.com>
2021-07-22 14:47:07 +0100

    [ADD] project paths

--- src/protrend/utils/settings.py
+++ src/protrend/utils/settings.py
@@ -1,2 +1,10 @@
+import os
+from pathlib import Path
+
 REQUEST_TIMEOUT: int = 30
 REQUEST_RETRIES: int = 3
+
+ROOT_PATH = Path(os.path.dirname(__file__)).parent
+EXTRACT_PATH = ROOT_PATH.joinpath('extract')
+STAGING_AREA_PATH: str = ROOT_PATH.joinpath('extract', 'staging_area')
+TRANSFORM_PATH = ROOT_PATH.joinpath('transform')
