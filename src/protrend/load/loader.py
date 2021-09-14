03a45e89fc28831beea5e85f9ddd7029680e808f
cruz-f <fernandocruz184@gmail.com>
2021-09-14 19:18:14 +0100

    [ADD] relationship loader

--- src/protrend/load/loader.py
+++ src/protrend/load/loader.py
@@ -2,10 +2,12 @@ import os
 from typing import List, Type
 
 import pandas as pd
+from neomodel import RelationshipManager
 
 from protrend.io.json import read_json_frame
 from protrend.load.settings import LoaderSettings
-from protrend.model.node import get_node_by_name
+from protrend.log.logger import Logger
+from protrend.model.node import get_node_by_name, get_nodes_relationships, connect_nodes
 from protrend.utils.settings import DATA_LAKE_PATH
 
 
@@ -34,10 +36,7 @@ class Loader:
             file_path = os.path.join(DATA_LAKE_PATH, self.source, self.version, file)
 
             if os.path.exists(file_path):
-
                 self._files.append(file_path)
-            else:
-                raise FileNotFoundError(f'Could not found file {file_path}')
 
     # --------------------------------------------------------
     # Static properties
@@ -126,8 +125,46 @@ class Loader:
         if df.empty:
             return
 
-        from_node_name = df['from_node'].iloc[0]
-        from_node = get_node_by_name(from_node_name)
+        from_node = df['from_node'].iloc[0]
+        from_node = get_node_by_name(from_node)
+        from_nodes = from_node.node_to_dict(to='node')
 
-        to_node_name = df['to_node'].iloc[0]
-        to_node = get_node_by_name(to_node_name)
+        to_node = df['to_node'].iloc[0]
+        to_node = get_node_by_name(to_node)
+        to_nodes = to_node.node_to_dict(to='node')
+
+        from_rels, to_rels = get_nodes_relationships(from_node=from_node, to_node=to_node)
+
+        for _, relationship in df.iterrows():
+
+            relationship = relationship.to_dict()
+
+            from_id = relationship['from_identifier']
+            from_node_instance = from_nodes.get(from_id)
+            to_id = relationship['to_identifier']
+            to_node_instance = to_nodes.get(to_id)
+
+            if not from_node_instance or not to_node_instance:
+                continue
+
+            # forward connection
+            for attr in from_rels:
+
+                try:
+                    connect_nodes(from_node=from_node_instance, to_node=to_node_instance, relationship=attr,
+                                  kwargs=relationship)
+
+                except:
+                    Logger.log.exception(f'Could not connect {from_node_instance.identifier} to '
+                                         f'{to_node_instance.identifier} using {relationship} relation')
+
+            # reverse connection
+            for attr in to_rels:
+
+                try:
+                    connect_nodes(from_node=to_node_instance, to_node=from_node_instance, relationship=attr,
+                                  kwargs=relationship)
+
+                except:
+                    Logger.log.exception(f'Could not connect {to_node_instance.identifier} to '
+                                         f'{from_node_instance.identifier} using {relationship} relation')
