16968e176ea175619f5ef2b4435cbbf1a5ef97ff
cruz-f <fernandocruz184@gmail.com>
2021-09-21 15:51:20 +0100

    [ADD] rfam collectf connections

--- src/protrend/transform/collectf/regulatory_family.py
+++ src/protrend/transform/collectf/regulatory_family.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-from protrend.io import read_from_stack, read_json_lines
-from protrend.model.model import RegulatoryFamily
-from protrend.transform.collectf.base import CollectfTransformer
+from protrend.io import read_from_stack, read_json_lines, read_json_frame
+from protrend.model.model import RegulatoryFamily, Regulator
+from protrend.transform.collectf.base import CollectfTransformer, CollectfConnector
+from protrend.transform.collectf.regulator import RegulatorTransformer
 from protrend.transform.processors import apply_processors, remove_white_space, rstrip, lstrip, \
-    remove_multiple_white_space, parse_collectf_description
+    remove_multiple_white_space, parse_collectf_description, to_list
 
 
 class RegulatoryFamilyTransformer(CollectfTransformer):
@@ -34,3 +35,28 @@ class RegulatoryFamilyTransformer(CollectfTransformer):
 
         self._stack_transformed_nodes(tf)
         return tf
+
+
+class RegulatoryFamilyToRegulatorConnector(CollectfConnector):
+    default_from_node = RegulatoryFamily
+    default_to_node = Regulator
+    default_connect_stack = {'rfam': 'integrated_regulatoryfamily.json', 'regulator': 'integrated_regulator.json'}
+
+    def connect(self):
+        regulator = read_from_stack(stack=self.connect_stack, file='regulator',
+                                    default_columns=RegulatorTransformer.columns, reader=read_json_frame)
+        regulator = regulator.rename(columns={'protrend_id': 'regulator_protrend_id'})
+
+        rfam = read_from_stack(stack=self.connect_stack, file='rfam',
+                               default_columns=RegulatoryFamilyTransformer.columns, reader=read_json_frame)
+        rfam = regulator.rename(columns={'protrend_id': 'rfam_protrend_id'})
+        rfam = apply_processors(rfam, regulon=to_list)
+        rfam = rfam.explode(column='regulon')
+
+        df = pd.merge(regulator, rfam, left_on='uniprot_accession', right_on='regulon')
+
+        from_identifiers = df['rfam_protrend_id'].tolist()
+        to_identifiers = df['regulator_protrend_id'].tolist()
+
+        df = self.make_connection(from_identifiers=from_identifiers,
+                                  to_identifiers=to_identifiers)
