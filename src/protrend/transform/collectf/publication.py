c4ff6522fbb888c9437d7b3c262418215e5625dd
cruz-f <fernandocruz184@gmail.com>
2021-09-21 11:40:07 +0100

    [FIX] collectf publication transformer

--- src/protrend/transform/collectf/publication.py
+++ src/protrend/transform/collectf/publication.py
@@ -7,25 +7,23 @@ from protrend.model.model import Publication
 from protrend.transform import PublicationDTO
 from protrend.transform.annotation import annotate_publications
 from protrend.transform.collectf.base import CollectfTransformer
-from protrend.transform.processors import apply_processors, parse_collectf_pubmed, to_int_str
+from protrend.transform.processors import apply_processors, to_int_str, to_list
 
 
 class PublicationTransformer(CollectfTransformer):
     default_node = Publication
-    default_node_factors = ('pmid', )
-    default_transform_stack = {'tf': 'TranscriptionFactor.json'}
+    default_node_factors = ('pmid',)
+    default_transform_stack = {'tfbs': 'TFBS.json'}
     default_order = 100
     columns = {'protrend_id',
-               'pmid', 'doi', 'title', 'author', 'year'
-               'name', 'family', 'description', 'regulon',
-               'pubmed'}
-    read_columns = {'name', 'family', 'description', 'regulon'}
+               'pmid', 'doi', 'title', 'author', 'year',
+               'tfbs_id', 'site_start', 'site_end', 'site_strand', 'mode', 'sequence',
+               'pubmed', 'organism', 'regulon', 'operon', 'gene', 'experimental_evidence'}
+    read_columns = {'tfbs_id', 'site_start', 'site_end', 'site_strand', 'mode', 'sequence', 'pubmed', 'organism',
+                    'regulon', 'operon', 'gene', 'experimental_evidence'}
 
-    def _transform_tf(self, tf: pd.DataFrame) -> pd.DataFrame:
-        df = self.drop_duplicates(df=tf, subset=['name'], perfect_match=True, preserve_nan=True)
-
-        df['pubmed'] = df['description']
-        df = apply_processors(df=df, pubmed=parse_collectf_pubmed)
+    def _transform_tfbs(self, tfbs: pd.DataFrame) -> pd.DataFrame:
+        df = apply_processors(df=tfbs, pubmed=to_list)
         df = df.explode(column='pubmed')
 
         df = self.create_input_value(df, col='pubmed')
@@ -45,10 +43,10 @@ class PublicationTransformer(CollectfTransformer):
         return pd.DataFrame([dto.to_dict() for dto in dtos])
 
     def transform(self):
-        tf = read_from_stack(stack=self.transform_stack, file='tf',
-                             default_columns=self.read_columns, reader=read_json_lines)
+        tfbs = read_from_stack(stack=self.transform_stack, file='tfbs',
+                               default_columns=self.read_columns, reader=read_json_lines)
 
-        df = self._transform_tf(tf)
+        df = self._transform_tfbs(tfbs)
         df = df.dropna(subset=['pubmed'])
         df = self.drop_duplicates(df=df, subset=['pubmed'], perfect_match=False, preserve_nan=False)
 
