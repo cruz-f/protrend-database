97c103428b03c92b0d8ae9f51ed61ec41c5fc4d5
cruz-f <fernandocruz184@gmail.com>
2021-10-13 16:05:16 +0100

    [ADD] publication connections for RegulonDB

--- src/protrend/transform/regulondb/publication.py
+++ src/protrend/transform/regulondb/publication.py
@@ -2,12 +2,17 @@ from typing import List
 
 import pandas as pd
 
-from protrend.io import read_from_stack, read_txt
-from protrend.model.model import Publication
+from protrend.io import read_from_stack, read_txt, read_json_frame
+from protrend.model.model import Publication, Regulator, TFBS, Promoter, Operon, Gene
 from protrend.transform import PublicationDTO
 from protrend.transform.annotation import annotate_publications
 from protrend.transform.processors import apply_processors, to_int_str
-from protrend.transform.regulondb.base import RegulondbTransformer
+from protrend.transform.regulondb.base import RegulondbTransformer, RegulondbConnector
+from protrend.transform.regulondb.gene import GeneTransformer
+from protrend.transform.regulondb.operon import OperonTransformer
+from protrend.transform.regulondb.promoter import PromoterTransformer
+from protrend.transform.regulondb.regulator import RegulatorTransformer
+from protrend.transform.regulondb.tfbs import TFBSTransformer
 from protrend.utils import SetList
 
 
@@ -60,3 +65,192 @@ class PublicationTransformer(RegulondbTransformer):
         df = apply_processors(df, pmid=to_int_str)
 
         self._stack_transformed_nodes(df)
+
+
+class PublicationToRegulatorConnector(RegulondbConnector):
+    default_from_node = Publication
+    default_to_node = Regulator
+    default_connect_stack = {'publication': 'integrated_publication.json',
+                             'regulator': 'integrated_regulator.json',
+                             'obj_ev_pub': 'object_ev_method_pub_link.txt'}
+
+    def connect(self):
+        publication = read_from_stack(stack=self._connect_stack, file='publication',
+                                      default_columns=PublicationTransformer.columns, reader=read_json_frame)
+        publication = publication[['protrend_id', 'publication_id']]
+        publication = publication.rename(columns={'protrend_id': 'publication_protrend_id'})
+
+        regulator = read_from_stack(stack=self._connect_stack, file='regulator',
+                                    default_columns=RegulatorTransformer.columns, reader=read_json_frame)
+        regulator = regulator[['protrend_id', 'transcription_factor_id', 'sigma_id', 'srna_id']]
+        regulator = regulator.rename(columns={'protrend_id': 'regulator_protrend_id'})
+
+        obj_ev_pub_cols = ['object_id', 'evidence_id', 'method_id', 'publication_id']
+        obj_ev_pub = read_from_stack(stack=self._connect_stack, file='obj_ev_pub',
+                                     default_columns=obj_ev_pub_cols, reader=read_txt,
+                                     names=obj_ev_pub_cols, skiprows=31)
+
+        obj_tf = pd.merge(obj_ev_pub, regulator, left_on='object_id', right_on='transcription_factor_id')
+        obj_sigma = pd.merge(obj_ev_pub, regulator, left_on='object_id', right_on='sigma_id')
+        obj_srna = pd.merge(obj_ev_pub, regulator, left_on='object_id', right_on='srna_id')
+
+        obj_reg = pd.concat([obj_tf, obj_sigma, obj_srna], axis=0)
+
+        reg_pub = pd.merge(obj_reg, publication, on='publication_id')
+        reg_pub = reg_pub.drop_duplicates(subset=['publication_protrend_id', 'regulator_protrend_id'])
+
+        from_identifiers = reg_pub['publication_protrend_id'].tolist()
+        to_identifiers = reg_pub['regulator_protrend_id'].tolist()
+
+        df = self.make_connection(from_identifiers=from_identifiers,
+                                  to_identifiers=to_identifiers)
+
+        self.stack_json(df)
+
+
+class PublicationToTFBSConnector(RegulondbConnector):
+    default_from_node = Publication
+    default_to_node = TFBS
+    default_connect_stack = {'publication': 'integrated_publication.json',
+                             'tfbs': 'integrated_tfbs.json',
+                             'obj_ev_pub': 'object_ev_method_pub_link.txt'}
+
+    def connect(self):
+        publication = read_from_stack(stack=self._connect_stack, file='publication',
+                                      default_columns=PublicationTransformer.columns, reader=read_json_frame)
+        publication = publication[['protrend_id', 'publication_id']]
+        publication = publication.rename(columns={'protrend_id': 'publication_protrend_id'})
+
+        tfbs = read_from_stack(stack=self._connect_stack, file='tfbs',
+                               default_columns=TFBSTransformer.columns, reader=read_json_frame)
+        tfbs = tfbs[['protrend_id', 'site_id']]
+        tfbs = tfbs.rename(columns={'protrend_id': 'tfbs_protrend_id'})
+
+        obj_ev_pub_cols = ['object_id', 'evidence_id', 'method_id', 'publication_id']
+        obj_ev_pub = read_from_stack(stack=self._connect_stack, file='obj_ev_pub',
+                                     default_columns=obj_ev_pub_cols, reader=read_txt,
+                                     names=obj_ev_pub_cols, skiprows=31)
+
+        obj_site = pd.merge(obj_ev_pub, tfbs, left_on='object_id', right_on='site_id')
+
+        tfbs_pub = pd.merge(obj_site, publication, on='publication_id')
+        tfbs_pub = tfbs_pub.drop_duplicates(subset=['publication_protrend_id', 'tfbs_protrend_id'])
+
+        from_identifiers = tfbs_pub['publication_protrend_id'].tolist()
+        to_identifiers = tfbs_pub['tfbs_protrend_id'].tolist()
+
+        df = self.make_connection(from_identifiers=from_identifiers,
+                                  to_identifiers=to_identifiers)
+
+        self.stack_json(df)
+
+
+class PublicationToPromoterConnector(RegulondbConnector):
+    default_from_node = Publication
+    default_to_node = Promoter
+    default_connect_stack = {'publication': 'integrated_publication.json',
+                             'promoter': 'integrated_promoter.json',
+                             'obj_ev_pub': 'object_ev_method_pub_link.txt'}
+
+    def connect(self):
+        publication = read_from_stack(stack=self._connect_stack, file='publication',
+                                      default_columns=PublicationTransformer.columns, reader=read_json_frame)
+        publication = publication[['protrend_id', 'publication_id']]
+        publication = publication.rename(columns={'protrend_id': 'publication_protrend_id'})
+
+        promoter = read_from_stack(stack=self._connect_stack, file='promoter',
+                                   default_columns=PromoterTransformer.columns, reader=read_json_frame)
+        promoter = promoter[['protrend_id', 'promoter_id']]
+        promoter = promoter.rename(columns={'protrend_id': 'promoter_protrend_id'})
+
+        obj_ev_pub_cols = ['object_id', 'evidence_id', 'method_id', 'publication_id']
+        obj_ev_pub = read_from_stack(stack=self._connect_stack, file='obj_ev_pub',
+                                     default_columns=obj_ev_pub_cols, reader=read_txt,
+                                     names=obj_ev_pub_cols, skiprows=31)
+
+        obj_promoter = pd.merge(obj_ev_pub, promoter, left_on='object_id', right_on='promoter_id')
+
+        promoter_pub = pd.merge(obj_promoter, publication, on='publication_id')
+        promoter_pub = promoter_pub.drop_duplicates(subset=['publication_protrend_id', 'promoter_protrend_id'])
+
+        from_identifiers = promoter_pub['publication_protrend_id'].tolist()
+        to_identifiers = promoter_pub['promoter_protrend_id'].tolist()
+
+        df = self.make_connection(from_identifiers=from_identifiers,
+                                  to_identifiers=to_identifiers)
+
+        self.stack_json(df)
+
+
+class PublicationToOperonConnector(RegulondbConnector):
+    default_from_node = Publication
+    default_to_node = Operon
+    default_connect_stack = {'publication': 'integrated_publication.json',
+                             'operon': 'integrated_operon.json',
+                             'obj_ev_pub': 'object_ev_method_pub_link.txt'}
+
+    def connect(self):
+        publication = read_from_stack(stack=self._connect_stack, file='publication',
+                                      default_columns=PublicationTransformer.columns, reader=read_json_frame)
+        publication = publication[['protrend_id', 'publication_id']]
+        publication = publication.rename(columns={'protrend_id': 'publication_protrend_id'})
+
+        operon = read_from_stack(stack=self._connect_stack, file='operon',
+                                 default_columns=OperonTransformer.columns, reader=read_json_frame)
+        operon = operon[['protrend_id', 'operon_id']]
+        operon = operon.rename(columns={'protrend_id': 'operon_protrend_id'})
+
+        obj_ev_pub_cols = ['object_id', 'evidence_id', 'method_id', 'publication_id']
+        obj_ev_pub = read_from_stack(stack=self._connect_stack, file='obj_ev_pub',
+                                     default_columns=obj_ev_pub_cols, reader=read_txt,
+                                     names=obj_ev_pub_cols, skiprows=31)
+
+        obj_operon = pd.merge(obj_ev_pub, operon, left_on='object_id', right_on='operon_id')
+
+        operon_pub = pd.merge(obj_operon, publication, on='publication_id')
+        operon_pub = operon_pub.drop_duplicates(subset=['publication_protrend_id', 'operon_protrend_id'])
+
+        from_identifiers = operon_pub['publication_protrend_id'].tolist()
+        to_identifiers = operon_pub['operon_protrend_id'].tolist()
+
+        df = self.make_connection(from_identifiers=from_identifiers,
+                                  to_identifiers=to_identifiers)
+
+        self.stack_json(df)
+
+
+class PublicationToGeneConnector(RegulondbConnector):
+    default_from_node = Publication
+    default_to_node = Gene
+    default_connect_stack = {'publication': 'integrated_publication.json',
+                             'gene': 'integrated_gene.json',
+                             'obj_ev_pub': 'object_ev_method_pub_link.txt'}
+
+    def connect(self):
+        publication = read_from_stack(stack=self._connect_stack, file='publication',
+                                      default_columns=PublicationTransformer.columns, reader=read_json_frame)
+        publication = publication[['protrend_id', 'publication_id']]
+        publication = publication.rename(columns={'protrend_id': 'publication_protrend_id'})
+
+        gene = read_from_stack(stack=self._connect_stack, file='gene',
+                               default_columns=GeneTransformer.columns, reader=read_json_frame)
+        gene = gene[['protrend_id', 'gene_id']]
+        gene = gene.rename(columns={'protrend_id': 'gene_protrend_id'})
+
+        obj_ev_pub_cols = ['object_id', 'evidence_id', 'method_id', 'publication_id']
+        obj_ev_pub = read_from_stack(stack=self._connect_stack, file='obj_ev_pub',
+                                     default_columns=obj_ev_pub_cols, reader=read_txt,
+                                     names=obj_ev_pub_cols, skiprows=31)
+
+        obj_gene = pd.merge(obj_ev_pub, gene, left_on='object_id', right_on='gene_id')
+
+        gene_pub = pd.merge(obj_gene, publication, on='publication_id')
+        gene_pub = gene_pub.drop_duplicates(subset=['publication_protrend_id', 'gene_protrend_id'])
+
+        from_identifiers = gene_pub['publication_protrend_id'].tolist()
+        to_identifiers = gene_pub['gene_protrend_id'].tolist()
+
+        df = self.make_connection(from_identifiers=from_identifiers,
+                                  to_identifiers=to_identifiers)
+
+        self.stack_json(df)
