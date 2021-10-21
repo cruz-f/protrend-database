349417510d28a687a7bb3e87b530b19a6272a1b2
cruz-f <fernandocruz184@gmail.com>
2021-10-21 09:32:36 +0100

    [ADD] evidence transformer coryneregnet; build regulation in base coryneregnet transformer

--- src/protrend/transform/coryneregnet/base.py
+++ src/protrend/transform/coryneregnet/base.py
@@ -1,9 +1,44 @@
+from typing import Dict
+
+import pandas as pd
+
+from protrend.io import read_from_stack, read_csv
 from protrend.transform import Transformer, Connector
+from protrend.utils import SetList
 
 
 class CoryneRegNetTransformer(Transformer):
     default_source = 'coryneregnet'
     default_version = '0.0.0'
+    default_regulation_stack = {'bsub': 'bsub_regulation.csv',
+                                'cglu': 'cglu_regulation.csv',
+                                'ecol': 'ecol_regulation.csv',
+                                'mtub': 'mtub_regulation.csv'}
+
+    default_regulation_columns = SetList(['TF_locusTag', 'TF_altLocusTag', 'TF_name', 'TF_role',
+                                          'TG_locusTag', 'TG_altLocusTag', 'TG_name', 'Operon',
+                                          'Binding_site', 'Role', 'Is_sigma_factor', 'Evidence', 'PMID', 'Source'])
+
+    taxa_to_organism_code = {'bsub': '224308',
+                             'cglu': '196627',
+                             'ecol': '511145',
+                             'mtub': '83332'}
+
+    def _build_regulations(self, regulation_stack: Dict[str, str] = None) -> pd.DataFrame:
+
+        if not regulation_stack:
+            regulation_stack = self.default_regulation_stack
+
+        dfs = []
+        for file in regulation_stack:
+            df = read_from_stack(stack=regulation_stack,
+                                 file=file,
+                                 default_columns=self.default_regulation_columns,
+                                 reader=read_csv)
+            df['taxa'] = self.taxa_to_organism_code[file]
+            dfs.append(df)
+
+        return pd.concat(dfs, axis=0)
 
 
 class CoryneRegNetConnector(Connector):
---
+++ src/protrend/transform/coryneregnet/evidence.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+from protrend.io import read_from_stack, read_csv
+from protrend.model.model import Evidence
+from protrend.transform.coryneregnet.base import CoryneRegNetTransformer
+from protrend.utils import SetList
+
+
+class EvidenceTransformer(CoryneRegNetTransformer):
+    default_node = Evidence
+    default_transform_stack = {'bsub': 'bsub_regulation.csv',
+                               'cglu': 'cglu_regulation.csv',
+                               'cglu_rna': 'cglu_rna.csv',
+                               'ecol': 'ecol_regulation.csv',
+                               'mtub': 'mtub_regulation.csv'}
+    default_order = 100
+    columns = SetList(['protrend_id',
+                       'name', 'description'])
+    cglu_rna_columns = SetList(['locus_tag', 'evidence', 'srna_class', 'start_position', 'end_position',
+                                'genome', 'orientation', 'evidence_functional', 'functional_rna', 'sequence'])
+
+    def _transform_evidence(self, regulation: pd.DataFrame, rna: pd.DataFrame) -> pd.DataFrame:
+        regulation = self.drop_duplicates(df=regulation, subset=['Evidence'], perfect_match=True, preserve_nan=True)
+        regulation = regulation.dropna(subset=['Evidence'])
+        regulation['name'] = regulation['Evidence']
+        regulation['description'] = None
+
+        rna = self.drop_duplicates(df=rna, subset=['evidence_functional'], perfect_match=True, preserve_nan=True)
+        rna = rna.dropna(subset=['evidence_functional'])
+        rna['name'] = rna['evidence_functional']
+        rna['description'] = None
+
+        df = pd.concat([regulation, rna], axis=0)
+
+        df = self.drop_duplicates(df=df, subset=['name'], perfect_match=True, preserve_nan=True)
+        df = df.dropna(subset=['name'])
+
+        return df
+
+    def transform(self):
+        regulation = self._build_regulations()
+        rna = read_from_stack(self.transform_stack, file='cglu_rna', default_columns=self.cglu_rna_columns,
+                              reader=read_csv, sep='\t')
+
+        evidence = self._transform_evidence(regulation, rna)
+
+        self._stack_transformed_nodes(evidence)
+        return evidence
