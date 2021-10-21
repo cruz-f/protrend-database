a4a921d01dbd486783de2ed02fc0f3ba15562726
cruz-f <fernandocruz184@gmail.com>
2021-10-21 15:10:52 +0100

    [ADD] TFBS transformer coryneregnet

--- src/protrend/transform/coryneregnet/base.py
+++ src/protrend/transform/coryneregnet/base.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from protrend.io import read_from_stack, read_csv
 from protrend.transform import Transformer, Connector
+from protrend.transform.processors import take_first, to_set_list
 from protrend.utils import SetList
 
 
@@ -15,9 +16,16 @@ class CoryneRegNetTransformer(Transformer):
                                 'ecol': 'ecol_regulation.csv',
                                 'mtub': 'mtub_regulation.csv'}
 
+    default_operon_stack = {'bsub': 'bsub_operon.csv',
+                            'cglu': 'cglu_operon.csv',
+                            'ecol': 'ecol_operon.csv',
+                            'mtub': 'mtub_operon.csv'}
+
     default_regulation_columns = SetList(['TF_locusTag', 'TF_altLocusTag', 'TF_name', 'TF_role',
                                           'TG_locusTag', 'TG_altLocusTag', 'TG_name', 'Operon',
                                           'Binding_site', 'Role', 'Is_sigma_factor', 'Evidence', 'PMID', 'Source'])
+
+    default_operon_columns = SetList(['Operon', 'Orientation', 'Genes'])
 
     taxa_to_organism_code = {'bsub': '224308',
                              'cglu': '196627',
@@ -35,6 +43,45 @@ class CoryneRegNetTransformer(Transformer):
                                  file=file,
                                  default_columns=self.default_regulation_columns,
                                  reader=read_csv)
+            df['taxonomy'] = self.taxa_to_organism_code[file]
+            dfs.append(df)
+
+        return pd.concat(dfs, axis=0)
+
+    def _read_operon(self, stack, file):
+        fixed_names = ['Operon', 'Orientation']
+        genes_names = [f'Genes{i}' for i in range(150)]
+        names = fixed_names + genes_names
+
+        df = read_from_stack(stack=stack,
+                             file=file,
+                             default_columns=self.default_operon_columns,
+                             reader=read_csv,
+                             names=names,
+                             engine='python',
+                             skiprows=1)
+        df['Operon'] = df['Operon'].str.replace('>', '')
+
+        df['Genes'] = df.iloc[:, 2:].values.tolist()
+
+        df = df[['Operon', 'Orientation', 'Genes']]
+        df = df.explode(column='Genes')
+        df = df.dropna(subset=['Genes'])
+
+        df = Transformer.group_by(df,
+                                  column='Operon',
+                                  aggregation={'Orientation': take_first},
+                                  default=to_set_list)
+        return df
+
+    def _build_operons(self, operon_stack: Dict[str, str] = None) -> pd.DataFrame:
+
+        if not operon_stack:
+            operon_stack = self.default_operon_stack
+
+        dfs = []
+        for file in operon_stack:
+            df = self._read_operon(operon_stack, file)
             df['taxonomy'] = self.taxa_to_organism_code[file]
             dfs.append(df)
 
---
+++ src/protrend/transform/coryneregnet/tfbs.py
@@ -0,0 +1,149 @@
+import pandas as pd
+
+from protrend.io import read_json_frame
+from protrend.io.utils import read_from_stack
+from protrend.model.model import TFBS
+from protrend.transform.coryneregnet.base import CoryneRegNetTransformer
+from protrend.transform.coryneregnet.gene import GeneTransformer
+from protrend.transform.processors import (apply_processors, to_list, to_str, operon_hash, site_hash, to_set_list,
+                                           take_first)
+from protrend.utils import SetList
+from protrend.utils.miscellaneous import is_null
+
+
+class TFBSTransformer(CoryneRegNetTransformer):
+    default_node = TFBS
+    default_transform_stack = {'bsub': 'bsub_regulation.csv',
+                               'cglu': 'cglu_regulation.csv',
+                               'ecol': 'ecol_regulation.csv',
+                               'mtub': 'mtub_regulation.csv',
+                               'gene': 'integrated_gene.json'}
+    default_order = 90
+    columns = SetList(['sequence', 'strand', 'start', 'stop', 'length', 'site_hash', 'protrend_id',
+                       'TF_locusTag', 'TF_altLocusTag', 'TF_name', 'TF_role',
+                       'TG_locusTag', 'TG_altLocusTag', 'TG_name', 'Operon',
+                       'Binding_site', 'Role', 'Is_sigma_factor', 'Evidence', 'PMID', 'Source', 'taxonomy',
+                       'Orientation', 'Genes'])
+
+    def _transform_tfbs(self, regulation: pd.DataFrame) -> pd.DataFrame:
+        # process multiple bs
+        def binding_site_sequences(sequences: str) -> list:
+            if is_null(sequences):
+                return []
+            return sequences.split(';')
+
+        regulation = apply_processors(regulation, Binding_site=binding_site_sequences)
+        regulation = regulation.explode(column='Binding_site')
+
+        # filter by duplicated target gene - sequences
+        regulation = self.drop_duplicates(df=regulation, subset=['TG_locusTag', 'Binding_site'],
+                                          perfect_match=True, preserve_nan=True)
+
+        # filter by nan
+        regulation = regulation.dropna(subset=['TG_locusTag', 'Binding_site'])
+
+        regulation['sequence'] = regulation['Binding_site']
+
+        return regulation
+
+    @staticmethod
+    def _tfbs_coordinates(tfbs: pd.DataFrame) -> pd.DataFrame:
+
+        def sequence_len(item):
+            if is_null(item):
+                return None
+
+            if isinstance(item, str):
+                return len(item)
+
+            return None
+
+        def strand(item):
+            if is_null(item):
+                return None
+
+            if item == '-':
+                return 'reverse'
+
+            if item == '+':
+                return 'forward'
+
+            return None
+
+        tfbs['length'] = tfbs['sequence'].map(sequence_len, na_action='ignore')
+        tfbs['start'] = None
+        tfbs['stop'] = None
+        tfbs['strand'] = tfbs['Orientation'].map(strand, na_action='ignore')
+
+        return tfbs
+
+    def _transform_gene(self) -> pd.DataFrame:
+        gene = read_from_stack(stack=self.transform_stack, file='gene', default_columns=GeneTransformer.columns,
+                               reader=read_json_frame)
+        gene = self.select_columns(gene, 'protrend_id', 'TG_locusTag')
+        gene = gene.rename(columns={'protrend_id': 'gene_protrend_id'})
+        return gene
+
+    def _transform_operon(self) -> pd.DataFrame:
+        # 'Operon', 'Orientation', 'Genes'
+        operon = self._build_operons()
+        operon_by_gene = operon.explode(column='Genes')
+        return operon_by_gene
+
+    def _transform_operon_gene(self) -> pd.DataFrame:
+        # 'Operon', 'Orientation', 'Genes'
+        operon = self._transform_operon()
+        # 'gene_protrend_id', 'TG_locusTag'
+        gene = self._transform_gene()
+        operon_gene = pd.merge(operon, gene, left_on='Genes', right_on='TG_locusTag')
+
+        aggregation = {'Orientation': take_first}
+        operon_gene = self.group_by(df=operon_gene, column='Operon', aggregation=aggregation, default=to_set_list)
+        return operon_gene
+
+    def _transform_operon_tfbs(self) -> pd.DataFrame:
+        # 'TF_locusTag', 'TF_altLocusTag', 'TF_name', 'TF_role', 'TG_locusTag', 'TG_altLocusTag', 'TG_name', 'Operon',
+        # 'Binding_site', 'Role', 'Is_sigma_factor', 'Evidence', 'PMID', 'Source', 'taxonomy'
+        regulation = self._build_regulations()
+        tfbs = self._transform_tfbs(regulation)
+        tfbs = tfbs.drop(columns=['Operon'])
+
+        # 'Operon', 'Orientation', 'Genes'
+        operon = self._transform_operon()
+
+        operon_tfbs = pd.merge(tfbs, operon, left_on='TG_locusTag', right_on='Genes')
+        return operon_tfbs
+
+    def transform(self):
+        # 'TF_locusTag', 'TF_altLocusTag', 'TF_name', 'TF_role', 'TG_locusTag', 'TG_altLocusTag', 'TG_name',
+        # 'Binding_site', 'Role', 'Is_sigma_factor', 'Evidence', 'PMID', 'Source', 'taxonomy',
+        # 'Operon', 'Orientation', 'Genes'
+        operon_tfbs = self._transform_operon_tfbs()
+        operon_tfbs = operon_tfbs.drop(columns=['Orientation', 'Genes'])
+
+        # 'Operon', 'Orientation', 'Genes', 'gene_protrend_id', 'TG_locusTag'
+        operon_gene = self._transform_operon_gene()
+        operon_gene = operon_gene.drop(columns=['TG_locusTag'])
+
+        operon_gene_tfbs = pd.merge(operon_tfbs, operon_gene, on='Operon')
+        operon_gene_tfbs = self._tfbs_coordinates(operon_gene_tfbs)
+
+        # filter by site hash: length + strand + start + genes
+        df = apply_processors(operon_gene_tfbs,
+                              sequence=[to_str, to_list],
+                              length=[to_str, to_list],
+                              strand=[to_str, to_list],
+                              start=[to_str, to_list],
+                              gene_protrend_id=[to_list, operon_hash, to_list])
+
+        operon_gene_tfbs['site_hash'] = df['sequence'] + df['length'] + df['strand'] + df['start'] + df[
+            'gene_protrend_id']
+        operon_gene_tfbs = apply_processors(operon_gene_tfbs, site_hash=site_hash)
+
+        operon_gene_tfbs = self.drop_duplicates(df=operon_gene_tfbs, subset=['site_hash'],
+                                                perfect_match=True, preserve_nan=True)
+        operon_gene_tfbs = operon_gene_tfbs.dropna(subset=['site_hash'])
+
+        self._stack_transformed_nodes(operon_gene_tfbs)
+
+        return operon_gene_tfbs
