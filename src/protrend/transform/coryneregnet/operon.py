02ed376eca97b4ec89d924063452a635afc34df3
cruz-f <fernandocruz184@gmail.com>
2021-10-22 11:04:08 +0100

    [ADD] operon and gene-tfbs connections for coryneregnet

--- src/protrend/transform/coryneregnet/operon.py
+++ src/protrend/transform/coryneregnet/operon.py
@@ -1,11 +1,9 @@
-from statistics import mode, StatisticsError
-
 import numpy as np
 import pandas as pd
 
 from protrend.io import read_from_stack, read_json_frame
-from protrend.model.model import Operon
-from protrend.transform.coryneregnet.base import CoryneRegNetTransformer
+from protrend.model.model import Operon, Gene, TFBS
+from protrend.transform.coryneregnet.base import CoryneRegNetTransformer, CoryneRegNetConnector
 from protrend.transform.coryneregnet.gene import GeneTransformer
 from protrend.transform.coryneregnet.tfbs import TFBSTransformer
 from protrend.transform.processors import (apply_processors, operon_name, flatten_set_list, to_list,
@@ -158,3 +156,78 @@ class OperonTransformer(CoryneRegNetTransformer):
         self._stack_transformed_nodes(operon_tfbs_gene)
 
         return operon_tfbs_gene
+
+
+class OperonToGeneConnector(CoryneRegNetConnector):
+    default_from_node = Operon
+    default_to_node = Gene
+    default_connect_stack = {'operon': 'integrated_operon.json'}
+
+    def connect(self):
+        operon = read_from_stack(stack=self._connect_stack, file='operon',
+                                 default_columns=OperonTransformer.columns, reader=read_json_frame)
+
+        operon = apply_processors(operon, genes=to_list)
+        operon = operon.explode('genes')
+
+        operon = operon.dropna(subset=['protrend_id', 'genes'])
+        operon = operon.drop_duplicates(subset=['protrend_id', 'genes'])
+
+        from_identifiers = operon['protrend_id'].tolist()
+        to_identifiers = operon['genes'].tolist()
+
+        df = self.make_connection(from_identifiers=from_identifiers,
+                                  to_identifiers=to_identifiers)
+
+        self.stack_json(df)
+
+
+class OperonToTFBSConnector(CoryneRegNetConnector):
+    default_from_node = Operon
+    default_to_node = TFBS
+    default_connect_stack = {'operon': 'integrated_operon.json'}
+
+    def connect(self):
+        operon = read_from_stack(stack=self._connect_stack, file='operon',
+                                 default_columns=OperonTransformer.columns, reader=read_json_frame)
+
+        operon = apply_processors(operon, tfbss=to_list)
+        operon = operon.explode('tfbss')
+
+        operon = operon.dropna(subset=['protrend_id', 'tfbss'])
+        operon = operon.drop_duplicates(subset=['protrend_id', 'tfbss'])
+
+        from_identifiers = operon['protrend_id'].tolist()
+        to_identifiers = operon['tfbss'].tolist()
+
+        df = self.make_connection(from_identifiers=from_identifiers,
+                                  to_identifiers=to_identifiers)
+
+        self.stack_json(df)
+
+
+class GeneToTFBSConnector(CoryneRegNetConnector):
+    default_from_node = Gene
+    default_to_node = TFBS
+    default_connect_stack = {'operon': 'integrated_operon.json'}
+
+    def connect(self):
+        operon = read_from_stack(stack=self._connect_stack, file='operon',
+                                 default_columns=OperonTransformer.columns, reader=read_json_frame)
+
+        operon = apply_processors(operon, genes=to_list, tfbss=to_list)
+        operon = operon.explode('tfbss')
+        operon = operon.explode('genes')
+
+        operon = operon.dropna(subset=['genes', 'tfbss'])
+        operon = operon.drop_duplicates(subset=['genes', 'tfbss'])
+
+        from_identifiers = operon['genes'].tolist()
+        to_identifiers = operon['tfbss'].tolist()
+        kwargs = dict(operon=operon['protrend_id'].tolist())
+
+        df = self.make_connection(from_identifiers=from_identifiers,
+                                  to_identifiers=to_identifiers,
+                                  kwargs=kwargs)
+
+        self.stack_json(df)
