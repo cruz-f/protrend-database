fbb683f23e5f44420555e3bfcf25831b7b87819d
cruz-f <fernandocruz184@gmail.com>
2021-12-13 15:08:52 +0000

    [ADD] build stack, build multi stack and build load stack universal methods to build transformers, connectors and loaders stacks

--- src/protrend/utils/__init__.py
+++ src/protrend/utils/__init__.py
@@ -3,7 +3,8 @@ from .settings import Settings
 from .graph import build_graph, build_edges, find_connected_nodes
 from .set_list import SetList
 from .default_property import DefaultProperty
-from .miscellaneous import WriteStack, is_null, build_stack, build_load_stack
+from .miscellaneous import is_null
+from .stack import WriteStack, MultiStack, build_stack, build_multi_stack, build_load_stack
 from .processors import apply_processors
 from .singleton import singleton
 from .request import request, read_response
