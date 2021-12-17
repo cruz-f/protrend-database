712675cdf9b0f8b2c5538b01bbcf59f55686c01f
cruz-f <fernandocruz184@gmail.com>
2021-12-17 19:49:11 +0000

    [ADD] adding the base regulatory interaction transformer

--- src/protrend/transform/transformer.py
+++ src/protrend/transform/transformer.py
@@ -688,3 +688,77 @@ class BaseOrganismTransformer(Transformer, register=False):
         self.stack_transformed_nodes(df)
 
         return df
+
+
+class BaseRegulatoryInteractionTransformer(Transformer, register=False):
+
+    @abstractmethod
+    def transform_network(self, network: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+    @abstractmethod
+    def transform_organism(self, organism: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+    @abstractmethod
+    def transform_regulator(self, regulator: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+    @abstractmethod
+    def transform_gene(self, gene: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+    def transform_tfbs(self, tfbs: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+    def transform_effector(self, effector: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+    def _transform(self,
+                   network: pd.DataFrame,
+                   organism: pd.DataFrame,
+                   organism_key: str,
+                   regulator: pd.DataFrame,
+                   regulator_key: str,
+                   gene: pd.DataFrame,
+                   gene_key: str,
+                   tfbs: pd.DataFrame = None,
+                   tfbs_key: str = None,
+                   effector: pd.DataFrame = None,
+                   effector_key: str = None,
+                   regulatory_effect_processor: Callable = None) -> pd.DataFrame:
+        network = self.transform_network(network)
+        organism = self.transform_organism(organism)
+        regulator = self.transform_regulator(regulator)
+        gene = self.transform_gene(gene)
+        if tfbs is not None:
+            tfbs = self.transform_tfbs(tfbs)
+        if effector is not None:
+            effector = self.transform_effector(effector)
+
+        regulatory_interaction = pd.merge(network, organism, on=organism_key)
+        regulatory_interaction = pd.merge(regulatory_interaction, regulator, on=regulator_key)
+        regulatory_interaction = pd.merge(regulatory_interaction, gene, on=gene_key)
+
+        if tfbs is not None:
+            regulatory_interaction = pd.merge(regulatory_interaction, tfbs, how='left', on=tfbs_key)
+
+        else:
+            regulatory_interaction = regulatory_interaction.assign(tfbs=None)
+
+        if effector is not None:
+            regulatory_interaction = pd.merge(regulatory_interaction, effector, how='left', on=effector_key)
+
+        else:
+            regulatory_interaction = regulatory_interaction.assign(effector=None)
+
+        if regulatory_effect_processor is not None:
+            regulatory_interaction = apply_processors(regulatory_interaction,
+                                                      regulatory_effect=regulatory_effect_processor)
+
+        regulatory_interaction = self.interaction_hash(regulatory_interaction)
+        return regulatory_interaction
+
+    @abstractmethod
+    def transform(self):
+        pass
