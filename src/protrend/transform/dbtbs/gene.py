bcc8c40e1758e1cfd98d0718aa68b76a395310c0
cruz-f <fernandocruz184@gmail.com>
2022-01-07 16:00:47 +0000

    [ADD] adding the transform GenBank record methods

--- src/protrend/transform/dbtbs/gene.py
+++ src/protrend/transform/dbtbs/gene.py
@@ -1,85 +1,12 @@
-from collections import defaultdict
-from typing import List, Dict, Union
-
 import pandas as pd
 from Bio import SeqIO
-from Bio.SeqRecord import SeqRecord
 
 from protrend.io import read_json_lines, read_from_stack
 from protrend.model import Gene
 from protrend.transform.dbtbs.base import DBTBSTransformer
+from protrend.transform.transformer import transform_sequence
 from protrend.utils import SetList
 from protrend.utils.processors import (rstrip, lstrip, apply_processors)
-
-
-def get_gene_name(qualifiers: Dict) -> Union[str, None]:
-    gene: List[str] = qualifiers.get('gene', [None])
-
-    if gene[0]:
-        return gene[0].lower()
-
-    return
-
-
-def get_locus(qualifiers: Dict) -> Union[str, None]:
-    locus: List[str] = qualifiers.get('locus_tag', [None])
-
-    if locus[0]:
-        return locus[0].lower()
-
-    return
-
-
-def get_genbank(qualifiers: Dict) -> Union[str, None]:
-    gb: List[str] = qualifiers.get('protein_id', [None])
-
-    if gb[0]:
-        return gb[0]
-
-    return
-
-
-def get_uniprot(qualifiers: Dict) -> Union[str, None]:
-    xrefs: List[str] = qualifiers.get('db_xref', [])
-
-    for xref in xrefs:
-
-        if 'UniProtKB/Swiss-Prot:' in xref:
-            return xref.replace('UniProtKB/Swiss-Prot:', '').rstrip().lstrip()
-
-    return
-
-
-def transform_sequence(sequence: SeqRecord) -> pd.DataFrame:
-
-    sequence_idx = defaultdict(SetList)
-
-    for feature in sequence.features:
-
-        if feature.type == 'CDS':
-
-            gene_name = get_gene_name(feature.qualifiers)
-
-            if gene_name:
-                sequence_idx[gene_name].append(gene_name)
-
-                locus = get_locus(feature.qualifiers)
-                sequence_idx[gene_name].append(locus)
-
-                gb_acc = get_genbank(feature.qualifiers)
-                sequence_idx[gene_name].append(gb_acc)
-
-                uniprot_acc = get_uniprot(feature.qualifiers)
-                sequence_idx[gene_name].append(uniprot_acc)
-
-    # filter out duplicated gene names
-    sequence_idx = {i: val for i, val in enumerate(sequence_idx.values())
-                    if len(val) == 4}
-
-    return pd.DataFrame.from_dict(sequence_idx,
-                                  orient='index',
-                                  columns=['name_lower', 'locus_tag',
-                                           'genbank_accession', 'uniprot_accession'])
 
 
 class GeneTransformer(DBTBSTransformer,
@@ -120,6 +47,7 @@ class GeneTransformer(DBTBSTransformer,
         return gene
 
     def transform(self):
+        # noinspection DuplicatedCode
         gene = read_from_stack(stack=self.transform_stack, key='gene',
                                columns=self.read_columns, reader=read_json_lines)
 
--- src/protrend/transform/transformer.py
+++ src/protrend/transform/transformer.py
@@ -1,9 +1,11 @@
 import os
 from abc import ABCMeta, abstractmethod
+from collections import defaultdict
 from functools import partial
-from typing import List, Type, Callable, Dict
+from typing import List, Type, Callable, Dict, Union
 
 import pandas as pd
+from Bio.SeqRecord import SeqRecord
 
 from protrend.annotation import (GeneDTO, annotate_genes,
                                  OrganismDTO, annotate_organisms,
@@ -680,6 +682,79 @@ class MultiStackTransformer(Transformer, register=False):
                              columns=columns,
                              reader=read_json_frame)
         return df
+
+
+# ----------------------------------------
+# Transform GenBank Record
+# ----------------------------------------
+def get_gene_name(qualifiers: Dict) -> Union[str, None]:
+    gene: List[str] = qualifiers.get('gene', [None])
+
+    if gene[0]:
+        return gene[0].lower()
+
+    return
+
+
+def get_locus(qualifiers: Dict) -> Union[str, None]:
+    locus: List[str] = qualifiers.get('locus_tag', [None])
+
+    if locus[0]:
+        return locus[0].lower()
+
+    return
+
+
+def get_genbank(qualifiers: Dict) -> Union[str, None]:
+    gb: List[str] = qualifiers.get('protein_id', [None])
+
+    if gb[0]:
+        return gb[0]
+
+    return
+
+
+def get_uniprot(qualifiers: Dict) -> Union[str, None]:
+    xrefs: List[str] = qualifiers.get('db_xref', [])
+
+    for xref in xrefs:
+
+        if 'UniProtKB/Swiss-Prot:' in xref:
+            return xref.replace('UniProtKB/Swiss-Prot:', '').rstrip().lstrip()
+
+    return
+
+
+def transform_sequence(sequence: SeqRecord) -> pd.DataFrame:
+
+    sequence_idx = defaultdict(SetList)
+
+    for feature in sequence.features:
+
+        if feature.type == 'CDS':
+
+            gene_name = get_gene_name(feature.qualifiers)
+
+            if gene_name:
+                sequence_idx[gene_name].append(gene_name)
+
+                locus = get_locus(feature.qualifiers)
+                sequence_idx[gene_name].append(locus)
+
+                gb_acc = get_genbank(feature.qualifiers)
+                sequence_idx[gene_name].append(gb_acc)
+
+                uniprot_acc = get_uniprot(feature.qualifiers)
+                sequence_idx[gene_name].append(uniprot_acc)
+
+    # filter out duplicated gene names
+    sequence_idx = {i: val for i, val in enumerate(sequence_idx.values())
+                    if len(val) == 4}
+
+    return pd.DataFrame.from_dict(sequence_idx,
+                                  orient='index',
+                                  columns=['name_lower', 'locus_tag',
+                                           'genbank_accession', 'uniprot_accession'])
 
 
 class BaseSourceTransformer(Transformer, register=False):
