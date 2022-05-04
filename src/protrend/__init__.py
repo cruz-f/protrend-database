2ac97b4730bcb7906749ec94dd903500e86531a0
Ana Pereira <anarspereira4@gmail.com>
2022-05-04 16:16:01 +0100

    [TEMP] comment of the python package imports

--- src/protrend/__init__.py
+++ src/protrend/__init__.py
@@ -1,8 +1,8 @@
-from .annotation import *
-from .bioapis import *
-from .extract import *
-from .transform import *
-from .load import *
-from .model import *
-from .pipeline import *
-from .runners import *
+# from .annotation import *
+# from .bioapis import *
+# from .extract import *
+# from .transform import *
+# from .load import *
+# from .model import *
+# from .pipeline import *
+# from .runners import *
