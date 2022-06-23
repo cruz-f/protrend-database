c53cd0f8953df94e22a36af222afd6a56ec346f6
Ana Rafaela Pereira <98289507+anarspereira@users.noreply.github.com>
2022-06-23 03:34:58 +0100

    [ADD] 2 ways to calculate descriptors with biopython (not tested)

--- src/protrend/descriptors/pssm.py
+++ src/protrend/descriptors/pssm.py
@@ -3,7 +3,19 @@ from Bio.Alphabet import IUPAC
 
 from protrend.transform.functional_tfbs.tfbs import TFBSTransformer
 
-def PSSM (motif):
+def PSSM_BP (motif):
     motif = TFBSTransformer.align_tfbs()
     descriptor = PositionSpecificScoringMatrix(alphabet=IUPAC, values=motif)
     return descriptor
+
+
+# ou
+
+from Bio import motifs
+
+def PSSM (motif):
+    motif = TFBSTransformer.align_tfbs()
+    m = motifs.create(motif)
+    pwm = m.counts.normalize(pseudocounts=0.5)
+    descriptor2 = pwm.log_odds()
+    return descriptor2
--- src/protrend/descriptors/pwm.py
+++ src/protrend/descriptors/pwm.py
@@ -3,7 +3,18 @@ from Bio.Alphabet import IUPAC
 
 from protrend.transform.functional_tfbs.tfbs import TFBSTransformer
 
-def PSSM (motif):
+def PWM_BP (motif):
     motif = TFBSTransformer.align_tfbs()
     descriptor = PositionWeightMatrix(alphabet=IUPAC, values=motif)
     return descriptor
+
+
+# ou
+
+from Bio import motifs
+
+def PWM (motif):
+    motif = TFBSTransformer.align_tfbs()
+    m = motifs.create(motif)
+    descriptor1 = m.counts.normalize(pseudocounts=0.5)
+return descriptor1
