3ec71133644f9a38ddb849c116fac0e10da16f5d
cruz-f <fernandocruz184@gmail.com>
2022-06-27 16:27:10 +0100

    [UPDATE] update of the promoter region transformer using the ftp server

--- src/protrend/transform/functional_tfbs/promoter.py
+++ src/protrend/transform/functional_tfbs/promoter.py
@@ -1,91 +1,139 @@
-def read_fasta(file_name):
-    fasta_file = SeqIO.parse(open(file_name), 'fasta')
-    fasta_seq = [seq for seq in fasta_file][0]
-    return fasta_seq
+import os
+from pathlib import Path
+import gzip
 
-def read_genbank(file_name):
-    gb_file = SeqIO.read(file_name, "gb")
-    return gb_file
+import pandas as pd
+from Bio import SeqIO
+from neo4j.exceptions import Neo4jError, DriverError
 
-class Promoter(FunctionalTFBSTransformer,
-                      source='functional_tfbs',
-                      version='0.0.0',
-                      node=TFBS,
-                      order=100,
-                      register=True):
+from protrend.bioapis import fetch_sequences_to_ncbi_ftp
+from protrend.model import Gene, Organism, PromoterRegion
+from protrend.transform.functional_tfbs.base import FunctionalTFBSTransformer
 
-    def fetch_nodes(self):
+
+def read_gb_file(gb_file_path: Path):
+    with gzip.open(gb_file_path, "rt") as handle:
+        for record in SeqIO.parse(handle, 'gb'):
+            return record
+
+
+class PromoterRegionTransformer(FunctionalTFBSTransformer,
+                                source='functional_tfbs',
+                                version='0.0.0',
+                                node=PromoterRegion,
+                                order=100,
+                                register=True):
+
+    @staticmethod
+    def fetch_genes():
         try:
-            return self.node.node_to_df()
+            return Gene.node_to_df()
         except (Neo4jError, DriverError) as e:
             print(e)
             return pd.DataFrame()
 
-    def retrieve_promoter_seq(self, in_gb, in_fasta, file_output, file2_output):
-        prom_output = ""
-        seq_output = ""
+    @staticmethod
+    def fetch_organisms():
+        try:
+            return Organism.node_to_df()
+        except (Neo4jError, DriverError) as e:
+            print(e)
+            return pd.DataFrame()
 
-        genome = read_fasta(in_fasta)
-        reverse_genome = genome[::-1]
-        GBrecord = read_genbank(in_gb)
+    def fetch_sequences(self, organisms: pd.DataFrame):
+        """
+        Fetches the sequences for the given organisms.
+        """
+        output_folder = os.path.join(self.write_path, 'sequences')
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
 
-        prom_len = 150
+        ftp_paths = organisms['genbank_ftp'].to_list()
+        protrend_ids = organisms['protrend_id'].to_list()
+        return fetch_sequences_to_ncbi_ftp(protrend_ids=protrend_ids, ftp_paths=ftp_paths, output_folder=output_folder)
 
-        for feature in GBrecord.features:
+    @staticmethod
+    def retrieve_promoter_sequences(gb_file: Path,
+                                    promoter_region_length: int = 150) -> pd.DataFrame:
+        """
+        Retrieves the promoter sequences for the given organisms.
+        :param gb_file: Path to the genbank file.
+        :param promoter_region_length: Length of the promoter region.
+        :return: Dataframe with the promoter sequences.
+        """
+        record = read_gb_file(gb_file)
 
-            if feature.type == 'gene':
-                my_start = feature.location.start.position  # Identifies the start position of the gene on the sense strand (5'-3')
-                my_end = feature.location.end.position  # Identifies the end position of the gene on the sense strand (5'-3')
-                if (my_start - prom_len) < 0:
-                    start_500 = 0
-                else:
-                    start_500 = my_start - prom_len
-                if (my_end + prom_len) > len(genome):
-                    end_500 = len(genome)
-                else:
-                    end_500 = my_end + prom_len
+        promoters = {'locus_tag': [],
+                     'uniprot_accession': [],
+                     'promoter_sequence': [],
+                     'start': [],
+                     'end': [],
+                     'strand': []}
+        for feature in record.features:
 
-                locus_tag = feature.qualifiers['locus_tag'][0]
-                feat_loc = str(feature.location)
+            if feature.type == 'CDS':
+                # Identifies the strand of the gene
+                strand = feature.strand
 
-                if feature.strand == -1:
-                    prom = genome[my_end:end_500].reverse_complement()
-                    gene_seq = reverse_genome[my_start:my_end].reverse_complement()
+                # Identifies the start position of the gene on the sense strand (5'-3')
+                gene_start = feature.location.start.position
 
-                elif feature.strand == 1:
-                    prom = genome[start_500:my_start]
-                    gene_seq = genome[my_start:my_end]
+                # Identifies the end position of the gene on the anti-sense strand (3'-5')
+                gene_end = feature.location.end.position
+
+                if strand == 1:
+                    promoter_region_start = int(gene_start - promoter_region_length)
+                    promoter_region_end = int(gene_start)
+
+                    if promoter_region_start < 0:
+                        promoter_region_start = 0
+
+                    promoter_region = record.seq[promoter_region_start:promoter_region_end]
+
+                elif strand == -1:
+                    promoter_region_start = int(gene_end)
+                    promoter_region_end = int(gene_end + promoter_region_length)
+
+                    if promoter_region_end > len(record.seq):
+                        promoter_region_end = len(record.seq)
+
+                    promoter_region = record.seq[promoter_region_start:promoter_region_end].reverse_complement()
 
                 else:
                     continue
 
-                prom_output += f">Promoter ___ {feat_loc} ___ {locus_tag} \n"
-                seq_output += f">Gene sequence ___ {feat_loc} ___ {locus_tag} \n"
+                locus_tag = feature.qualifiers.get('locus_tag', [None])[0]
+                uniprot_id = feature.qualifiers.get('protein_id', [None])[0]
 
-                if len(prom.seq) == prom_len:
-                    prom_output += str(prom.seq) + "\n\n"
+                promoters['locus_tag'].append(locus_tag)
+                promoters['uniprot_accession'].append(uniprot_id)
+                promoters['promoter_sequence'].append(str(promoter_region))
+                promoters['start'].append(promoter_region_start)
+                promoters['end'].append(promoter_region_end)
+                promoters['strand'].append(strand)
 
-                else:
-                    diff = prom_len - len(prom.seq)
-                    complement = 'A' * diff
-                    prom_output += complement + str(prom.seq) + "\n\n"
-
-                seq_output += str(gene_seq.seq) + "\n\n"
-
-        file = open(file_output, 'w')
-        file.write(prom_output)
-        file2 = open(file2_output, 'w')
-        file2.write(seq_output)
-        file.close()
-        file2.close()
+        return pd.DataFrame(promoters)
 
     def transform(self) -> pd.DataFrame:
-        tfbs = self.fetch_nodes()
-        promoters = read_promoters(source=self.source, version=self.version, columns=[])
+        organisms = self.fetch_organisms()
+        sequences = self.fetch_sequences(organisms)
 
-        aligned_tfbs = self.align_tfbs(tfbs, promoters)
+        promoters = []
+        for sequence in sequences:
+            promoter = self.retrieve_promoter_sequences(sequence)
+            promoters.append(promoter)
 
-        final_tfbs = self.calculate_descriptors(aligned_tfbs)
+        promoters = pd.concat(promoters)
+        promoters['locus_tag_merge'] = promoters['locus_tag'].str.lower()
 
-        self.stack_transformed_nodes(final_tfbs)
-        return final_tfbs
+        genes = self.fetch_genes()
+        genes = genes[['locus_tag', 'uniprot_accession']]
+        genes['locus_tag_merge'] = genes['locus_tag_merge'].str.lower()
+
+        promoters = pd.merge(promoters, genes, on='locus_tag_merge', suffixes=('_promoters', '_genes'))
+        promoters = promoters.drop(columns=['locus_tag_promoters', 'uniprot_accession_promoters', 'locus_tag_merge'])
+        promoters = promoters.rename(columns={'locus_tag_genes': 'locus_tag',
+                                              'uniprot_accession_genes': 'uniprot_accession'})
+
+        self.stack_transformed_nodes(promoters)
+        return promoters
