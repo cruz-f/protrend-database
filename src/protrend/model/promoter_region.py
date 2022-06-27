2e22bd6415b5df22a5318d68047d3dafa86e8659
cruz-f <fernandocruz184@gmail.com>
2022-06-27 16:27:19 +0100

    [UPDATE] update of the promoter region model

--- src/protrend/model/promoter_region.py
+++ src/protrend/model/promoter_region.py
@@ -1,20 +1,24 @@
 from neomodel import StringProperty, IntegerProperty, RelationshipTo, ZeroOrOne
 
-from protrend.utils.processors import rstrip, lstrip, lower_case, to_nan, to_int_str
+from protrend.utils.processors import rstrip, lstrip, lower_case, to_nan, to_int_str, to_str
 from .base import BaseNode
-from .relationships import BaseRelationship, BASE_REL_TYPE
-from .utils import help_text
+from .relationships import BaseRelationship, BASE_REL_TYPE, SOURCE_REL_TYPE, SourceRelationship
+from .utils import help_text, choices
 
 
 class PromoterRegion(BaseNode):
     entity = 'PRO'
-    node_factors = {'pmid': [to_int_str, lower_case, rstrip, lstrip, to_nan]} # compor
+    node_factors = {'uniprot_accession': [to_str, lower_case, rstrip, lstrip, to_nan],
+                    'locus_tag': [to_str, lower_case, rstrip, lstrip, to_nan]}
 
     # properties
-    promoter_sequence = StringProperty(help_text=help_text.promoter_sequence)
     locus_tag = StringProperty(required=True, unique_index=True, max_length=100, help_text=help_text.locus_tag)
+    locus_tag_factor = StringProperty(required=True, unique_index=True, max_length=100,
+                                      help_text=help_text.required_name)
     uniprot_accession = StringProperty(unique_index=True, max_length=50, help_text=help_text.uniprot_accession)
-    start = IntegerProperty(help_text=help_text.start) # uso o start e stop já existente ou crio específicos para promoters?
+    uniprot_accession_factor = StringProperty(unique_index=True, max_length=50, help_text=help_text.uniprot_accession)
+    promoter_sequence = StringProperty(help_text=help_text.promoter_sequence)
+    start = IntegerProperty(help_text=help_text.start)
     end = IntegerProperty(help_text=help_text.stop)
     strand = StringProperty(choices=choices.strand, help_text=help_text.strand)
 
