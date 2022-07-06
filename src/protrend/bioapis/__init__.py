cc184c7461854397995d1f02ee09e708dd01d3a2
cruz-f <fernandocruz184@gmail.com>
2022-07-06 18:22:26 +0100

    [ADD] adding more filters to ncbi assembly search for assembly identifiers

--- src/protrend/bioapis/__init__.py
+++ src/protrend/bioapis/__init__.py
@@ -2,7 +2,7 @@ from .compound import KEGGCompound
 from .entrez import entrez_search, entrez_summary, entrez_fetch
 from .gene import NCBIGene
 from .kegg import fetch_kegg_list, indexing_kegg_list, search_kegg_list
-from .ncbi_ftp import fetch_sequences_to_ncbi_ftp
+from .ncbi_ftp import fetch_sequences
 from .organism import NCBITaxonomyOrganism
 from .pathway import KEGGPathway
 from .protrein import UniProtProtein, NCBIProtein
--- src/protrend/bioapis/ncbi_ftp.py
+++ src/protrend/bioapis/ncbi_ftp.py
@@ -13,6 +13,7 @@ def fetch_sequences(ftp_path: str, file_name_or_path: Path):
     :param file_name_or_path: Output file path.
     :return: None
     """
+    time.sleep(Settings.request_sleep)
     ftp_path = ftp_path.replace('ftp://ftp.ncbi.nlm.nih.gov/', '')
     accession = ftp_path.split('/')[-1]
 
@@ -36,32 +37,10 @@ def fetch_sequences(ftp_path: str, file_name_or_path: Path):
     ftp.quit()
 
 
-def fetch_sequences_to_ncbi_ftp(protrend_ids: List[str], ftp_paths: List[str], output_folder: str):
-    """
-    Fetch sequences from NCBI FTP server.
-    :param ftp_paths: List of FTP paths.
-    :param output_folder: Output folder.
-    :param protrend_ids: List of protrend IDs.
-    :return: List of output files.
-    """
-    if not protrend_ids:
-        protrend_ids = [None] * len(ftp_paths)
-
-    output_folder = Path(output_folder)
-
-    output_sequences = []
-    for ftp_path, protrend_id in zip(ftp_paths, protrend_ids):
-        file_path = output_folder.joinpath(f'{protrend_id}.gbff.gz')
-        fetch_sequences(ftp_path=ftp_path, file_name_or_path=file_path)
-        output_sequences.append(file_path)
-        time.sleep(Settings.request_sleep)
-
-    return output_sequences
-
-
 if __name__ == '__main__':
-    fetch_sequences_to_ncbi_ftp(
-        ftp_paths=['ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/021/485/GCA_000021485.1_ASM2148v1'],
-        output_folder=r'C:\Users\BiSBII\OneDrive - Universidade do Minho\PhD\Protrend\main\protrend-database\useless',
-        protrend_ids=['PRT.ORG.0000001']
-    )
+    pass
+    # fetch_sequences_to_ncbi_ftp(
+    #     ftp_paths=['ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/021/485/GCA_000021485.1_ASM2148v1'],
+    #     output_folder=r'C:\Users\BiSBII\OneDrive - Universidade do Minho\PhD\Protrend\main\protrend-database\useless',
+    #     protrend_ids=['PRT.ORG.0000001']
+    # )
