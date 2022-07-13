5b49cf115c44708aff0ca2cbed63a757e74bd182
cruz-f <fernandocruz184@gmail.com>
2022-07-13 16:09:31 +0100

    [FIX] fixing the promoter region data model

--- src/protrend/model/promoter_region.py
+++ src/protrend/model/promoter_region.py
@@ -8,21 +8,19 @@ from .utils import help_text, choices
 
 class PromoterRegion(BaseNode):
     entity = 'PRO'
-    node_factors = {'uniprot_accession': [to_str, lower_case, rstrip, lstrip, to_nan],
-                    'locus_tag': [to_str, lower_case, rstrip, lstrip, to_nan]}
+    node_factors = {'locus_tag': [to_str, lower_case, rstrip, lstrip, to_nan]}
 
     # properties
     locus_tag = StringProperty(required=True, unique_index=True, max_length=100, help_text=help_text.locus_tag)
     locus_tag_factor = StringProperty(required=True, unique_index=True, max_length=100,
-                                      help_text=help_text.required_name)
-    uniprot_accession = StringProperty(unique_index=True, max_length=50, help_text=help_text.uniprot_accession)
-    uniprot_accession_factor = StringProperty(unique_index=True, max_length=50, help_text=help_text.uniprot_accession)
-    promoter_sequence = StringProperty(help_text=help_text.promoter_sequence)
+                                      help_text=help_text.locus_tag)
+    gene = StringProperty(required=True, max_length=100, help_text=help_text.gene_id)
+    sequence = StringProperty(help_text=help_text.promoter_sequence)
     start = IntegerProperty(help_text=help_text.start)
-    end = IntegerProperty(help_text=help_text.stop)
+    stop = IntegerProperty(help_text=help_text.stop)
     strand = StringProperty(choices=choices.strand, help_text=help_text.strand)
 
     # relationships
     data_source = RelationshipTo('.source.Source', SOURCE_REL_TYPE, cardinality=ZeroOrOne, model=SourceRelationship)
-    gene = RelationshipTo('.gene.Gene', BASE_REL_TYPE, cardinality=ZeroOrOne, model=BaseRelationship)
+    data_gene = RelationshipTo('.gene.Gene', BASE_REL_TYPE, cardinality=ZeroOrOne, model=BaseRelationship)
     organism = RelationshipTo('.organism.Organism', BASE_REL_TYPE, cardinality=ZeroOrOne, model=BaseRelationship)
