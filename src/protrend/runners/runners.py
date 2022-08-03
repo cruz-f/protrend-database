7aa6487cfe10f6771ee9f11ac8b4eea624281616
cruz-f <fernandocruz184@gmail.com>
2022-08-03 13:01:55 +0100

    [SET] set the runner to motif only

--- src/protrend/runners/runners.py
+++ src/protrend/runners/runners.py
@@ -137,47 +137,47 @@ if __name__ == "__main__":
     # # run_logger('collectf_logger')
     # run_pipeline(source='collectf', version='0.0.1')
 
-    # ----------------------------------------------------
-    # RegPrecise
-    # ----------------------------------------------------
-    # run_logger('regprecise_logger')
-    run_pipeline(source='regprecise', version='0.0.0')
+    # # ----------------------------------------------------
+    # # RegPrecise
+    # # ----------------------------------------------------
+    # # run_logger('regprecise_logger')
+    # run_pipeline(source='regprecise', version='0.0.0')
 
-    # ----------------------------------------------------
-    # Abasy
-    # ----------------------------------------------------
-    # run_logger('abasy_logger')
-    run_pipeline(source='abasy', version='0.0.0')
+    # # ----------------------------------------------------
+    # # Abasy
+    # # ----------------------------------------------------
+    # # run_logger('abasy_logger')
+    # run_pipeline(source='abasy', version='0.0.0')
 
-    # ----------------------------------------------------
-    # Literature
-    # ----------------------------------------------------
-    # run_logger('literature_logger')
-    run_pipeline(source='literature', version='0.0.0')
+    # # ----------------------------------------------------
+    # # Literature
+    # # ----------------------------------------------------
+    # # run_logger('literature_logger')
+    # run_pipeline(source='literature', version='0.0.0')
 
-    # ----------------------------------------------------
-    # CoryneRegNet
-    # ----------------------------------------------------
-    # run_logger('coryneregnet_logger')
-    run_pipeline(source='coryneregnet', version='0.0.0')
+    # # ----------------------------------------------------
+    # # CoryneRegNet
+    # # ----------------------------------------------------
+    # # run_logger('coryneregnet_logger')
+    # run_pipeline(source='coryneregnet', version='0.0.0')
 
-    # ----------------------------------------------------
-    # DBTBS
-    # ----------------------------------------------------
-    # run_logger('dbtbs_logger')
-    run_pipeline(source='dbtbs', version='0.0.4')
+    # # ----------------------------------------------------
+    # # DBTBS
+    # # ----------------------------------------------------
+    # # run_logger('dbtbs_logger')
+    # run_pipeline(source='dbtbs', version='0.0.4')
 
-    # ----------------------------------------------------
-    # RegulonDB
-    # ----------------------------------------------------
-    # run_logger('regulondb_logger')
-    run_pipeline(source='regulondb', version='0.0.0')
+    # # ----------------------------------------------------
+    # # RegulonDB
+    # # ----------------------------------------------------
+    # # run_logger('regulondb_logger')
+    # run_pipeline(source='regulondb', version='0.0.0')
 
-    # ----------------------------------------------------
-    # OperonDB
-    # ----------------------------------------------------
-    # run_logger('operondb_logger')
-    run_pipeline(source='operondb', version='0.0.0')
+    # # ----------------------------------------------------
+    # # OperonDB
+    # # ----------------------------------------------------
+    # # run_logger('operondb_logger')
+    # run_pipeline(source='operondb', version='0.0.0')
 
     # ----------------------------------------------------
     # Promoter Region
@@ -191,12 +191,12 @@ if __name__ == "__main__":
     # run_logger('moitf_logger')
     run_pipeline(source='motif', version='0.0.0')
 
-    # ----------------------------------------------------
-    # Standardizer
-    # ----------------------------------------------------
-    # run_logger('standardizer_logger')
-    # run_database(install_all_labels=True)
-    run_pipeline(source='standardizer', version='0.0.0',
-                 transform=True,
-                 connect=False,
-                 load=False)
+    # # ----------------------------------------------------
+    # # Standardizer
+    # # ----------------------------------------------------
+    # # run_logger('standardizer_logger')
+    # # run_database(install_all_labels=True)
+    # run_pipeline(source='standardizer', version='0.0.0',
+    #              transform=True,
+    #              connect=False,
+    #              load=False)
